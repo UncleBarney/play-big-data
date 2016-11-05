1b8f687efb68bd323b3e15c71b4be9b29a96772e
Hechen Gao <hechen.gao@autodesk.com>
2016-11-05 12:21:32 -0700

    minor upgrade

--- nodejs/public/main.js
+++ nodejs/public/main.js
@@ -1,11 +1,54 @@
 $(function () {
 
+    var data_points = [];
+
     $("#chart").height($(window).height() - $("#header").height() * 2);
 
-    var data_points = [{
-        values: [],
-        key: 'AAPL'
-    }];
+    $(document.body).on('click', '.stock-label', function () {
+        "use strict";
+        var symbol = $(this).text();
+        $.ajax({
+            url: 'http://localhost:5000/' + symbol,
+            type: 'DELETE'
+        });
+
+        $(this).remove();
+        var i = getSymbolIndex(symbol, data_points);
+        data_points.splice(i, 1);
+        console.log(data_points);
+    });
+
+    $("#add-stock-button").click(function () {
+        "use strict";
+        var symbol = $("#stock-symbol").val();
+
+        $.ajax({
+            url: 'http://localhost:5000/' + symbol,
+            type: 'POST'
+        });
+
+        $("#stock-symbol").val("");
+        data_points.push({
+            values: [],
+            key: symbol
+        });
+
+        $("#stock-list").append(
+            "<a class='stock-label list-group-item small'>" + symbol + "</a>"
+        );
+
+        console.log(data_points);
+    });
+
+    function getSymbolIndex(symbol, array) {
+        "use strict";
+        for (var i = 0; i < array.length; i++) {
+            if (array[i].key == symbol) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     var chart = nv.models.lineChart()
         .interpolate('monotone')
@@ -42,15 +85,18 @@ $(function () {
         var parsed = JSON.parse(message);
         var timestamp = parsed['timestamp'];
         var average = parsed['average'];
+        var symbol = parsed['symbol'];
         var point = {};
         point.x = timestamp;
         point.y = average;
 
         console.log(point);
 
-        data_points[0].values.push(point);
-        if (data_points[0].length > 100) {
-            data_points[0].values.shift()
+        var i = getSymbolIndex(symbol, data_points);
+
+        data_points[i].values.push(point);
+        if (data_points[i].length > 100) {
+            data_points[i].values.shift();
         }
         loadGraph();
     }
